75d826cdfb5a2a4464f04da91aa29fb1378528fa
bluemzzn <synthiawithsyren@gmail.com>
2025-05-18 16:23:16 +0700

    timeline responsive+testimonial[done]

--- event.js
+++ event.js
@@ -48,9 +48,7 @@ const observer = new IntersectionObserver((entries) =>{
     if(entry.isIntersecting){
       entry.target.classList.add('show');
     }
-    else{
-       entry.target.classList.remove('hidden');
-    }
+
   });
 });
 
